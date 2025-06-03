2c6a75a194f60458e4ffc13c1d25430901384011
Thakkasila Lalitha <thakkasilalalitha@gmail.com>
2025-06-03 16:22:08 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text' , {
+    strings: ['Frontend Developer','Frontend Developer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1000,
+    loop: true,
+});
